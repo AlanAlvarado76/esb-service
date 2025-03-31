1675acd22ee661b25ad70d642c83a60745b613b4
AlanAlvarado76 <luigialv76@gmail.com>
2025-03-31 07:38:44 -0600

    init commit 4

--- src/main/java/com/utd/ti/soa/esb_service/controller/ESBController.java
+++ src/main/java/com/utd/ti/soa/esb_service/controller/ESBController.java
@@ -8,10 +8,10 @@ import com.utd.ti.soa.esb_service.model.User;
 import java.util.List;
 
 @RestController
-@RequestMapping("http://user-service.railway.internal:6000/api/v1/esb")
+@RequestMapping("/api/users")
 public class ESBController {
     private final WebClient webClient = WebClient.builder()
-            .baseUrl("user-service.railway.internal/api/v1/esb")
+            .baseUrl("https://user-service.railway.internal:6000/api/users")
             .defaultHeader("Content-Type", "application/json")
             .build();
 
